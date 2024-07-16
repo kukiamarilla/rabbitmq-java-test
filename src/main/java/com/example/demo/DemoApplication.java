93f432956dcf40beeb856e63ed32a488ea0ec71d
Isaac Amarilla <sc.amarilla@gmail.com>
2024-07-15 20:18:27 -0400

    First Commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,79 @@
+package com.example.demo;
+
+import org.aopalliance.aop.Advice;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.config.RetryInterceptorBuilder;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import io.micrometer.core.instrument.MeterRegistry;
+
+@SpringBootApplication
+public class DemoApplication {
+
+  static final String topicExchangeName = "spring-boot-exchange";
+
+  static final String queueName = "spring-boot";
+
+  @Bean
+  Queue queue() {
+    return new Queue(queueName, true);
+  }
+
+  @Bean
+  TopicExchange exchange() {
+    return new TopicExchange(topicExchangeName);
+  }
+
+  @Bean
+  Binding binding(Queue queue, TopicExchange exchange) {
+    return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
+  }
+
+  @Bean
+  SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+      MessageListenerAdapter listenerAdapter) {
+    SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+    container.setConnectionFactory(connectionFactory);
+    container.setQueueNames(queueName);
+    container.setMessageListener(listenerAdapter);
+    
+    return container;
+  }
+
+  @Bean
+public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+    SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+    factory.setConnectionFactory(connectionFactory);
+    factory.setConcurrentConsumers(3);
+    factory.setMaxConcurrentConsumers(10);
+    factory.setDefaultRequeueRejected(false);
+    factory.setAdviceChain(new Advice[] { RetryInterceptorBuilder.stateless()
+        .maxAttempts(5)
+        .backOffOptions(1000, 2.0, 10000)
+        .build() });
+    return factory;
+}
+  @Bean
+  public MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+    return registry -> registry.config().commonTags("application", "my-app");
+  }
+
+  @Bean
+  MessageListenerAdapter listenerAdapter(Receiver receiver) {
+    return new MessageListenerAdapter(receiver, "receiveMessage");
+  }
+
+  public static void main(String[] args) throws InterruptedException {
+    SpringApplication.run(DemoApplication.class, args).close();
+  }
+
+}
